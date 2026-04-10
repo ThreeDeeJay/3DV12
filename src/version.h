e326a1cdde81e9d51688843f8079babeecac8f36
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2026-04-10 15:47:44 -0400

    Minor fixes

--- src/version.h
+++ src/version.h
@@ -5,7 +5,7 @@
 
 // Increment this integer for every release.
 // It appears in log headers and compiled artifacts.
-#define VERSION        6
-#define VERSION_STRING "6"
+#define VERSION        7
+#define VERSION_STRING "7"
 #define PROJECT_NAME   "3DV12"
 #define PROJECT_FULL   "3DV12 v" VERSION_STRING " – D3D12 Stereo Shader Patcher"
